e99aa04a04837b1e72a1d0fb8c3d618f9d24531b
obaidch9019 <91061353+obaidch9019@users.noreply.github.com>
2022-04-03 21:28:07 +0500

    media-root

--- BWMM/settings.py
+++ BWMM/settings.py
@@ -132,7 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
